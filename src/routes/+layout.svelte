e00464695570784c22f55a8740d2291d83e6f494
Safayet <hossainsafayet187@gmail.com>
2025-11-06 00:07:04 +0600

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,23 @@
   import Navbar from "../components/Navbar.svelte";
   import "../app.css";
   import Footer from "../components/Footer.svelte";
+
   import { fade } from "svelte/transition";
 </script>
+
+<!-- Navbar -->
+<div class="flex flex-col transition-container bg-[#1D232A]">
+  <Navbar />
+
+  <main class="flex-grow" transition:fade={{ duration: 500 }}>
+    <section id="home">
+      <slot />
+      <!-- Renders current page content -->
+    </section>
+  </main>
+
+  <Footer />
+</div>
 
 <style>
   /* Custom styles for your transition */
@@ -12,16 +27,3 @@
     overflow: hidden;
   }
 </style>
-
-<!-- Navbar -->
-<div class="flex flex-col transition-container bg-[#1D232A]">
-  <Navbar />
-
-  <main class="flex-grow" transition:fade={{ duration: 500 }}>
-    <section id="home">
-      <slot />  <!-- Renders current page content -->
-    </section>
-  </main>
-
-  <Footer />
-</div>
